91dea1d2abbe7c41129941e1b905e73169b22a64
Garrett Blake McQuigg <garrettmcquigg@garretts-air.lan>
2023-06-20 22:42:37 -0500

    final touches

--- pba-health-server/db.js
+++ pba-health-server/db.js
@@ -1,12 +1,12 @@
 const { Client } = require('pg');
-const { postgresPassword } = require('./config');
+const { postgresPassword, postgresUsername } = require('./config');
 
 let DB_URI;
 
 if (process.env.NODE_ENV === 'test') {
   DB_URI = 'postgresql:///pbausers_test';
 } else {
-  DB_URI = `postgresql://gbmcquigg:${postgresPassword}@localhost:5432/pbausers`;
+  DB_URI = `postgresql://${postgresUsername}:${postgresPassword}@localhost:5432/pbausers`;
 }
 
 let db = new Client({
--- pba-health-server/example.config.js
+++ pba-health-server/example.config.js
@@ -2,6 +2,8 @@ const BCRYPT_WORK_FACTOR = 12;
 
 const SECRET_KEY = process.env.SECRET_KEY || 'secret';
 
-const postgresPassword = 'password';
+const postgresUsername = 'yourUsername';
 
-module.exports = { BCRYPT_WORK_FACTOR, SECRET_KEY, postgresPassword };
+const postgresPassword = 'yourPassword';
+
+module.exports = { BCRYPT_WORK_FACTOR, SECRET_KEY, postgresPassword, postgresUsername };
